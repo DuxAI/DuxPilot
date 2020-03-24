91d616e02241d835cd3cc954dc312d17125b955a
anatbalzam <anatbaz@gmail.com>
2020-03-24 11:07:38 +0200

    fixed bug of User instead of UserToken

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 exports.Channel = require("./channels");
 exports.Msg = require("./msg");
 exports.User = require("./users");
+exports.UserToken = require("./users.tokens");
 exports.Calender = require("./calenderEvent");
